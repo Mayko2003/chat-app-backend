ca39212dbab069f826b19ac7a7e35c4a60b52068
Mayko2003 <maykox34f@gmail.com>
2023-02-23 14:21:27 -0300

    chore: add user routes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
-
+import userController from '../controllers/user.controller'
+import authMiddleware from '../middlewares/authMiddleware'
 const router: Router = Router()
 
 //routes
+
+router.get('/', authMiddleware, userController.getAllUsers)
+router.get('/id/:id', authMiddleware, userController.getUserById)
+router.get('/username/:username', authMiddleware, userController.getUserByUsername)
+router.post('/create', userController.createUser)
+router.put('/update/:id', authMiddleware, userController.updateUser)
+router.delete('/inactive/:id', authMiddleware, userController.deleteUser)
 
 export default router
