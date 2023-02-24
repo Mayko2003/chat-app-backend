2b04480814dba0995a757e1790391979c12eb475
Mayko2003 <maykox34f@gmail.com>
2023-02-24 13:36:22 -0300

    chore: init socket.io

--- index.ts
+++ index.ts
@@ -3,12 +3,21 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import connectDB from './config/database';
 import routes from './routes';
+import { Server } from "socket.io";
+import http from 'http';
+
 
 // env
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 // app
 const app: Express = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 // middlewares
 app.use(express.json());
@@ -16,7 +25,7 @@ app.use(cors());
 
 
 // routes
-app.use('/api', routes );
+app.use('/api', routes);
 
 // server
 
@@ -26,8 +35,8 @@ const HOST = process.env.HOST || 'localhost';
 const main = async () => {
     try {
         await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on: ${HOST}:${PORT}`);
+        server.listen(PORT, () => {
+            console.log(`Server running on http://${HOST}:${PORT}`);
         });
     } catch (error) {
         console.log(error);
