5c9f52d94c29e512e79210fd4cfe8518cdd34be8
Mayko2003 <maykox34f@gmail.com>
2023-02-24 13:25:51 -0300

    chore: add message routes

--- routes/message.routes.ts
+++ routes/message.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
+import messageController from '../controllers/message.controller'
+import authMiddleware from '../middlewares/authMiddleware'
+import checkStatusMiddleware from '../middlewares/checkStatusMiddleware'
 
 const router: Router = Router()
 
 //routes
+router.get('/', authMiddleware, messageController.getAllMessages)
+router.get('/conversation/:username', checkStatusMiddleware, authMiddleware, messageController.getConversation)
+router.post('/send/:username', authMiddleware, checkStatusMiddleware, messageController.sendMessage)
 
 export default router
