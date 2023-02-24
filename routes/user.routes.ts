71d6c3cccb291150d42960df18c77a52a801f96d
Mayko2003 <maykox34f@gmail.com>
2023-02-24 13:26:24 -0300

    chore: add check status middleware to param routes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import userController from '../controllers/user.controller'
 import authMiddleware from '../middlewares/authMiddleware'
+import checkStatusMiddleware from '../middlewares/checkStatusMiddleware'
 const router: Router = Router()
 
 //routes
 
 router.get('/', authMiddleware, userController.getAllUsers)
-router.get('/id/:id', authMiddleware, userController.getUserById)
-router.get('/username/:username', authMiddleware, userController.getUserByUsername)
+router.get('/id/:id', authMiddleware, checkStatusMiddleware, userController.getUserById)
+router.get('/username/:username', authMiddleware, checkStatusMiddleware, userController.getUserByUsername)
 router.post('/create', userController.createUser)
-router.put('/update/:id', authMiddleware, userController.updateUser)
-router.delete('/inactive/:id', authMiddleware, userController.deleteUser)
+router.put('/update/:id', authMiddleware, checkStatusMiddleware, userController.updateUser)
+router.delete('/inactive/:id', authMiddleware, checkStatusMiddleware, userController.deleteUser)
 router.get('/active', authMiddleware, userController.getActiveUsers)
 
 export default router
