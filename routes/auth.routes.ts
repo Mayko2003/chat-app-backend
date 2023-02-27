c00ba8e6208eb23cb30746e9fa6e04b51ce5001b
Mayko2003 <maykox34f@gmail.com>
2023-02-27 13:01:14 -0300

    chore: add route for user logged

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import authController from '../controllers/auth.controller'
+import authMiddleware from '../middlewares/authMiddleware'
 
 const router: Router = Router()
 
 //routes
 router.post('/login', authController.login)
+router.get('/me', authMiddleware, authController.getLoggedUser)
 
 export default router
