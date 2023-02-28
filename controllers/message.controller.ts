607aa88ab2e716f0572a434c38245a8a7f186341
Mayko2003 <maykox34f@gmail.com>
2023-02-28 13:39:00 -0300

    chore: emit message for two users

--- controllers/message.controller.ts
+++ controllers/message.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import Message from '../models/message';
 import User from '../models/user';
 import handleHttpError from '../utils/handleHttpError';
+import { Server } from 'socket.io';
 
 const messageController = {
 
@@ -20,15 +21,21 @@ const messageController = {
     sendMessage: async (req: Request, res: Response) => {
         try {
             const username = req.params.username;
-            const fromUser = req.body.loggedUserId;
+            const fromUser = req.body.loggedUser;
 
             const toUser = await User.findOne({ username });
 
             const message = await Message.create({
                 toUser: toUser?._id,
-                fromUser,
+                fromUser: fromUser?._id,
                 message: req.body.message,
             });
+
+            const io: Server = req.app.get('io');
+            const clients: Map<any, any> = req.app.get('clients');
+
+
+            io.to(clients.get(username)).to(clients.get(fromUser.username)).emit('message', message);
 
             res.status(200).json({
                 status: 200,
@@ -44,16 +51,16 @@ const messageController = {
     getConversation: async (req: Request, res: Response) => {
         try {
             const username = req.params.username;
-            const fromUser = req.body.loggedUserId;
+            const fromUser = req.body.loggedUser;
 
             const toUser = await User.findOne({ username });
 
             const messages = await Message.find({
                 $or: [
-                    { toUser: toUser?._id, fromUser },
-                    { toUser: fromUser, fromUser: toUser?._id }
+                    { toUser: toUser?._id, fromUser: fromUser?._id },
+                    { toUser: fromUser?._id, fromUser: toUser?._id }
                 ]
-            });
+            }).populate('fromUser', 'username').populate('toUser', 'username');
 
             res.status(200).json({
                 status: 200,
